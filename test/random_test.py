cb5b9f4584aa90f31db1cbc78c7d079e577881ac
Atri Rathore <rathoreatri@gmail.com>
2025-02-09 16:36:50 +0530

    added files

--- test/random_test.py
+++ test/random_test.py
@@ -14,7 +14,7 @@ if os.path.exists(output_dir):
 os.makedirs(output_dir)
 
 # Load model
-model_path = r"E:\Stream_Censor\StreamClear\model\openlogo.pt"
+model_path = r"/StreamClear/assets/datasets/openlogo.pt"
 model = torch.hub.load('ultralytics/yolov5', 'custom', path=model_path)
 
 # Set model parameters
